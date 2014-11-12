eac29b31705b91c3bf811eb9c22af1b3b49a57c9
Spencer Sharpe <jspencersharpe@gmail.com>
2014-11-12 09:03:15 -0600

    wip

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {   
-$('button'.click, function() {
-  var $nameBox = $("#NameForm").val();
- alert($nameBox);
 
-  	});
+
+  
+$("#NameForm").submit(function(event){
+	  event.preventDefault();
+    function splitName ($nameBox) {
+     }
+
+     var $nameBox = $("#text_field").val();
+     var preamble = "Your " + "name " + "is " + $nameBox;
+     var GiveMe = "Give " + "Me " + "...";
+     var $caps = $nameBox.toUpperCase;
+     function toArray($caps) {
+       return $caps.split("");
+     }
+     
+    
+     $("#cheers").append(preamble); 
+     $("#cheers").after(GiveMe); 
+     $("#cheers").after($caps[0]);
+  });
 });
 
 
 
-   
 
